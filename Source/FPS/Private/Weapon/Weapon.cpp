b46b2c1a1d636b6e0baa3c05c918a0d76b3d978a
Fabian <fabianfroding@proton.me>
2026-08-09 13:18:42 +0200

    Weapon base class

---
+++ Source/FPS/Private/Weapon/Weapon.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright Fabian Fröding.
+
+#include "Weapon/Weapon.h"
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true; // Only replicate when the owner is visible.
+	
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh1P");
+	// Important optimization to restrict tick to owning player.
+	Mesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh1P->bReceivesDecals = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetHiddenInGame(true);
+	SetRootComponent(Mesh1P);
+	
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh3P");
+	Mesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh3P->bReceivesDecals = false;
+	Mesh3P->CastShadow = true;
+	Mesh3P->SetupAttachment(Mesh1P);
+	Mesh3P->SetHiddenInGame(true);
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/FPS/Public/Weapon/Weapon.h
@@ -0,0 +1,33 @@
+﻿// Copyright Fabian Fröding.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include <GameFramework/Actor.h>
+
+#include "Weapon.generated.h"
+
+UCLASS()
+class FPS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	// Weapon mesh first-person view.
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+	
+	// Weapon mesh third-person view.
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+
+public:
+	AWeapon();
+	
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	USkeletalMeshComponent* GetMesh3P() const { return Mesh3P; }
+
+protected:
+	virtual void BeginPlay() override;
+	
+};
