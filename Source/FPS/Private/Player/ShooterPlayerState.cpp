b22daa087f2f4dafb5a23b08cf8f88f5a9119202
Fabian <fabianfroding@proton.me>
2026-09-06 17:10:13 +0200

    Added player state class

---
+++ Source/FPS/Private/Player/ShooterPlayerState.cpp
@@ -0,0 +1,101 @@
+﻿// Copyright Fabian Fröding.
+
+#include "Player/ShooterPlayerState.h"
+
+
+AShooterPlayerState::AShooterPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+	
+	ScoredElims = 0;
+	Defeats = 0;
+	Hits = 0;
+	Misses = 0;
+	bOnStreak = false;
+	HeadShotElims = 0;
+	HighestStreak = 0;
+	RevengeElims = 0;
+	DethroneElims = 0;
+	ShowStopperElims = 0;
+	bFirstBlood = false;
+	bWinner = false;
+}
+
+void AShooterPlayerState::AddScoredElim()
+{
+	++ScoredElims;
+}
+
+void AShooterPlayerState::AddDefeat()
+{
+	++Defeats;
+}
+
+void AShooterPlayerState::AddHit()
+{
+	++Hits;
+}
+
+void AShooterPlayerState::AddMiss()
+{
+	++Misses;
+}
+
+void AShooterPlayerState::AddHeadShotElim()
+{
+	++HeadShotElims;
+}
+
+void AShooterPlayerState::AddSequentialElim(int32 SequenceCount)
+{
+	if (SequentialElims.Contains(SequenceCount))
+	{
+		SequentialElims[SequenceCount]++;
+	}
+	else
+	{
+		SequentialElims.Add(SequenceCount, 1);
+	}
+	
+	// Logic to prevent a higher sequential elim to count as a lower sequential elim (triple also counting as double etc).
+	for (auto& Elem : SequentialElims)
+	{
+		if (Elem.Key < SequenceCount && Elem.Value > 0)
+		{
+			Elem.Value--;
+		}
+	}
+}
+
+void AShooterPlayerState::UpdateHighestStreak(int32 StreakCount)
+{
+	if (StreakCount > HighestStreak)
+	{
+		HighestStreak = StreakCount;
+	}
+}
+
+void AShooterPlayerState::AddRevengeElim()
+{
+	++RevengeElims;
+}
+
+void AShooterPlayerState::AddDethroneElim()
+{
+	++DethroneElims;
+}
+
+void AShooterPlayerState::AddShowStopperElim()
+{
+	++ShowStopperElims;
+}
+
+void AShooterPlayerState::GetFirstBlood()
+{
+	bFirstBlood = true;
+}
+
+void AShooterPlayerState::IsNowWinner()
+{
+	bWinner = true;
+}
---
+++ Source/FPS/Public/Player/ShooterPlayerState.h
@@ -0,0 +1,45 @@
+﻿// Copyright Fabian Fröding.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "ShooterPlayerState.generated.h"
+
+UCLASS()
+class FPS_API AShooterPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	AShooterPlayerState();
+	
+	void AddScoredElim();
+	void AddDefeat();
+	void AddHit();
+	void AddMiss();
+	void AddHeadShotElim();
+	void AddSequentialElim(int32 SequenceCount);
+	void UpdateHighestStreak(int32 StreakCount);
+	void AddRevengeElim();
+	void AddDethroneElim();
+	void AddShowStopperElim();
+	void GetFirstBlood();
+	void IsNowWinner();
+	
+private:
+	int32 ScoredElims;
+	int32 Defeats;
+	int32 Hits;
+	int32 Misses;
+	bool bOnStreak; // Number of eliminations since we spawned.
+	int32 HeadShotElims;
+	TMap<int, int32> SequentialElims; // Multiple eliminations within short period of time (double/tripe kills etc).
+	int32 HighestStreak;
+	int32 RevengeElims;
+	int32 DethroneElims;
+	int32 ShowStopperElims; // "Shutdowns".
+	bool bFirstBlood;
+	bool bWinner;
+	
+};
