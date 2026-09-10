3b45604b1878705384a3e59ef3c36376d73b8762
Fabian <fabianfroding@proton.me>
2026-09-10 22:35:50 +0200

    Special elim widget base class

---
+++ Source/FPS/Private/UI/SpecialElimWidget.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright Fabian Fröding.
+
+#include "UI/SpecialElimWidget.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void USpecialElimWidget::InitializeWidget(const FString& ElimMessage, UTexture2D* ElimTexture)
+{
+	if (IsValid(Text_ElimMessage))
+	{
+		Text_ElimMessage->SetText(FText::FromString(ElimMessage));
+	}
+	if (IsValid(Image_ElimType))
+	{
+		Image_ElimType->SetBrushFromTexture(ElimTexture);
+	}
+}
+
+void USpecialElimWidget::CenterWidget(UUserWidget* Widget, float VerticalRatio)
+{
+	if (!IsValid(Widget)) return;
+	const FVector2D ViewportSize = UWidgetLayoutLibrary::GetViewportSize(Widget);
+	const float VerticalFraction = VerticalRatio == 0.f ? 1.f : VerticalRatio * 2.f;
+	FVector2D CenterPosition(ViewportSize.X / 2.f, VerticalFraction * ViewportSize.Y / 2.f);
+	Widget->SetAlignmentInViewport(FVector2D(0.5f, 0.5f)); // Align widget center to center of viewport.
+	Widget->SetPositionInViewport(CenterPosition, true);
+}
---
+++ Source/FPS/Public/UI/SpecialElimWidget.h
@@ -0,0 +1,31 @@
+﻿// Copyright Fabian Fröding.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "SpecialElimWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class FPS_API USpecialElimWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_ElimMessage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_ElimType;
+	
+public:
+	void InitializeWidget(const FString& ElimMessage, UTexture2D* ElimTexture);
+	
+	UFUNCTION(BlueprintCallable)
+	static void CenterWidget(UUserWidget* Widget, float VerticalRatio = 0.f);
+	
+};
